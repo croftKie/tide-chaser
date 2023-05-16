f97a5f6e2632f031f30008df4a7ee74d952dd8d6
Kieran Croft <kiecroft@gmail.com>
2023-05-16 09:14:52 +0100

    fetch data refactor

--- js/fetchData.js
+++ js/fetchData.js
@@ -1,32 +1,30 @@
-import { stormGlassKey } from "../config.js";
 import { mapScript } from "./map.js";
 import { setDailies } from "./dom/dailiesDom.js";
 import { setHourlies } from "./dom/hourlyDom.js";
 import { setSurfData, setWeatherData } from "./dom/breakdownDom.js";
 import { dailyAverage, hourlyData } from "./utils/calculations.js";
 import { initMenu, popup } from "./dom/menuDom.js";
-import {hideAnims, breakdownAnimHide} from "./anims/mainAnims.js";
 
-// Initialisation function on load.
+// fetches data from oceanography data api
 export const fetchData = async (lng, lat) => {
+    // provided parameters for data.
     const params = 'airTemperature,humidity,cloudCover,visibility,precipitation,waveHeight,wavePeriod,windDirection,windSpeed,waterTemperature';
     try {
         const base64Encoded = btoa(`https://api.stormglass.io/v2/weather/point?lat=${lat}&lng=${lng}&params=${params}`);
         const {data} = await axios.get(`https://surf-proxy.vercel.app?query=${base64Encoded}`);
         data.hours.length = 24 * 7;
+        // functions to separate daily average data and per hour data.
         const daily = dailyAverage(data.hours);
         const hourly = hourlyData(data.hours);
         let mode = 0;
+        // if it is the first load by user, run map and menu initialisation
         if (mode === 0) {
             mapScript();
             initMenu();
-            popup(daily, hourly);
-            dataManager(daily, hourly);
             mode = 1;
         } else {
-            popup(daily, hourly);
-            dataManager(daily, hourly);
-        }
+        popup(daily, hourly);
+        dataManager(daily, hourly);
     } catch (err) {
         toast("Surf data not available, try again later");
     }
