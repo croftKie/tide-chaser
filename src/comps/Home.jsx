0b25bc41fd78b6ae8b4a22ebc87d7b9a73c8ce92
croftkie <croftkie@proton.me>
2024-01-07 23:17:16 +0000

    a homepage that I'm kind of happy with

--- src/comps/Home.jsx
+++ src/comps/Home.jsx
@@ -1,16 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
 import HomeNav from "./homeComps/HomeNav";
 import HomeHero from "./homeComps/HomeHero";
 import Features from "./homeComps/Features";
 import Pricing from "./homeComps/Pricing";
 import "../css/home.css";
+import gsap from "gsap";
+import { useRef } from "react";
 
 const Home = () => {
+  const caroRef = useRef();
+  const [currentStep, setCurrentStep] = useState(0);
+  const [currentValue, setCurrentvalue] = useState(0);
+  const scroll = (step) => {
+    gsap.to(caroRef.current, {
+      x: `-${step}vw`,
+      duration: 1.5,
+      ease: "elastic.out(1,0.8)",
+    });
+    setCurrentvalue(step);
+  };
+
+  // console.log(currentStep, currentValue);
   return (
     <div className="home bg">
       <HomeNav />
       <div className="carousel">
-        <div className="inner">
+        <div ref={caroRef} className="inner">
           <div className="item">
             <HomeHero />
           </div>
@@ -22,13 +37,19 @@ const Home = () => {
           </div>
         </div>
       </div>
-      <div className="controls">
-        <button>
-          <h6>Back</h6>
-        </button>
-        <button>
-          <h6>Forward</h6>
-        </button>
+      <div className="selectors">
+        <div
+          onClick={() => scroll(0)}
+          className={currentValue === 0 ? "selector active" : "selector"}
+        ></div>
+        <div
+          onClick={() => scroll(100)}
+          className={currentValue === 100 ? "selector active" : "selector"}
+        ></div>
+        <div
+          onClick={() => scroll(200)}
+          className={currentValue === 200 ? "selector active" : "selector"}
+        ></div>
       </div>
     </div>
   );
